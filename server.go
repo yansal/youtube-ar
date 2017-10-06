11e41e5b820fa57d5efb7c28480a621b12a6764d
Yann Salaün <yannsalaun1@gmail.com>
2017-10-06 22:59:10 +0200

    Remove youtube-ar-client code

--- server.go
+++ server.go
@@ -51,8 +51,6 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.errorsHandler(w, r)
 	case strings.HasPrefix(p, "/detail/"):
 		s.detailHandler(w, r)
-	case p == "/_status/":
-		s.statusHandler(w, r)
 	default:
 		http.NotFound(w, r)
 	}
@@ -115,20 +113,6 @@ func (s *server) detailHandler(w http.ResponseWriter, r *http.Request) {
 	if err := s.tmpl.ExecuteTemplate(w, "detail.html", job); err != nil {
 		log.Print(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func (s *server) statusHandler(w http.ResponseWriter, r *http.Request) {
-	var jobs []Job
-	if err := s.db.Select(&jobs, s.queries["select_running.sql"]); err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if len(jobs) > 0 {
-		fmt.Fprint(w, "running")
-	} else {
-		fmt.Fprint(w, "idle")
 	}
 }
 
