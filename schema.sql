a426af7a0db7009f982f78e7fae4bf4710c31753
Yann Salaün <yannsalaun1@gmail.com>
2017-10-25 00:13:53 +0200

    fmt (#10)

--- schema.sql
+++ schema.sql
@@ -1,40 +1,40 @@
-BEGIN;
+begin;
 
-CREATE TYPE job_status AS ENUM ('running', 'done', 'error');
-CREATE TABLE jobs (
-    id serial PRIMARY KEY,
-    url text NOT NULL,
-    status job_status DEFAULT 'running' NOT NULL,
-    started_at timestamp with time zone DEFAULT now() NOT NULL,
+create type job_status as enum ('running', 'done', 'error');
+create table jobs (
+    id serial primary key,
+    url text not null,
+    status job_status default 'running' not null,
+    started_at timestamp with time zone default now() not null,
     downloaded_at timestamp with time zone,
     uploaded_at timestamp with time zone,
     output text,
     error text,
-    retries integer DEFAULT 0 NOT NULL,
+    retries integer default 0 not null,
     geoip jsonb,
     torlog text,
     feed xml,
-    CONSTRAINT status_running CHECK (status <> 'running' OR (error IS NULL AND uploaded_at IS NULL)),
-    CONSTRAINT status_done CHECK (status <> 'done' OR (error IS NULL AND output IS NOT NULL AND uploaded_at IS NOT NULL)),
-    CONSTRAINT status_error CHECK (status <> 'error' OR (error IS NOT NULL AND uploaded_at IS NULL))
+    constraint status_running check (status <> 'running' or (error is null and uploaded_at is null)),
+    constraint status_done check (status <> 'done' or (error is null and output is not null and uploaded_at is not null)),
+    constraint status_error check (status <> 'error' or (error is not null and uploaded_at is null))
 );
 
--- Forbid more than one running job per url
-CREATE UNIQUE INDEX jobs_url_status_running_idx ON jobs (url) WHERE status = 'running';
+-- forbid more than one running job per url
+create unique index jobs_url_status_running_idx on jobs (url) where status = 'running';
 
--- Notify worker after insert
-CREATE FUNCTION
+-- notify worker after insert
+create function
   notify_jobs()
-  RETURNS trigger
-  AS $$
-  BEGIN
-    NOTIFY jobs;
-    RETURN NULL;
-  END;
-$$ LANGUAGE plpgsql;
+  returns trigger
+  as $$
+  begin
+    notify jobs;
+    return null;
+  end;
+$$ language plpgsql;
 
-CREATE TRIGGER notify_jobs AFTER INSERT
-  ON jobs
-  EXECUTE PROCEDURE notify_jobs();
+create trigger notify_jobs after insert
+  on jobs
+  execute procedure notify_jobs();
 
-COMMIT;
+commit;
