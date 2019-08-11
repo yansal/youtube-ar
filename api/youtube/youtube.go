91c965286a76f18b5c2ebd374481b2ff281541e8
Yann Salaün <yann@ulule.com>
2019-08-11 20:35:55 +0200

    make youtube.Client a concrete type

--- api/youtube/youtube.go
+++ api/youtube/youtube.go
@@ -11,30 +11,27 @@ import (
 	loghttp "github.com/yansal/youtube-ar/api/log/http"
 )
 
-// Client is the client interface.
-type Client interface {
-	GetVideosFromPlaylist(context.Context, string) ([]Video, error)
-}
-
 // Video is the video resource.
 type Video struct {
 	ID string
 }
 
 // New returns a new Client.
-func New(log log.Logger) Client {
-	return &client{
+func New(log log.Logger) *Client {
+	return &Client{
 		apiKey: os.Getenv("YOUTUBE_API_KEY"),
 		client: loghttp.Wrap(new(http.Client), log),
 	}
 }
 
-type client struct {
+// A Client is a youtube client.
+type Client struct {
 	apiKey string
 	client *http.Client
 }
 
-func (c *client) GetVideosFromPlaylist(ctx context.Context, playlist string) ([]Video, error) {
+// GetVideosFromPlaylist gets videos from a youtube playlist.
+func (c *Client) GetVideosFromPlaylist(ctx context.Context, playlist string) ([]Video, error) {
 	g := getPlaylistItemsRequest{playlistID: playlist}
 	var videos []Video
 	for {
@@ -59,7 +56,7 @@ type getPlaylistItemsRequest struct {
 	playlistID string
 }
 
-func (c *client) getPlaylistItems(ctx context.Context, g getPlaylistItemsRequest) (*playlistItems, error) {
+func (c *Client) getPlaylistItems(ctx context.Context, g getPlaylistItemsRequest) (*playlistItems, error) {
 	u, err := url.Parse("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50")
 	if err != nil {
 		return nil, err
