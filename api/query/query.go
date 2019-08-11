e45987f3f5fb50360bf7cfe32a8715fdf67f58cb
Yann Salaün <yann@ulule.com>
2019-08-11 14:39:47 +0200

    bump github.com/yansal/query

--- api/query/query.go
+++ api/query/query.go
@@ -9,23 +9,23 @@ import (
 // ParseURLs parses v and returns a new URLs.
 func ParseURLs(v url.Values) (*URLs, error) {
 	q, err := query.Validate(v,
-		query.WithIntParam("limit"),
-		query.WithIntParam("cursor"),
-		query.WithStringsParam("status", []string{"pending", "processing", "failure", "success"}),
+		query.IntParam("limit"),
+		query.IntParam("cursor"),
+		query.StringsParam("status", []string{"pending", "processing", "failure", "success"}),
 	)
 	if err != nil {
 		return nil, err
 	}
 	var u URLs
-	if cursor, ok := q.Params["cursor"]; ok {
+	if cursor, ok := q["cursor"]; ok {
 		u.Cursor = cursor.(int64)
 	}
-	if limit, ok := q.Params["limit"]; !ok {
+	if limit, ok := q["limit"]; !ok {
 		u.Limit = DefaultLimit
 	} else {
 		u.Limit = limit.(int64)
 	}
-	if status, ok := q.Params["status"]; ok {
+	if status, ok := q["status"]; ok {
 		u.Status = status.([]string)
 	}
 	return &u, nil
@@ -33,15 +33,12 @@ func ParseURLs(v url.Values) (*URLs, error) {
 
 // ParseLogs parses v and returns a new Logs.
 func ParseLogs(v url.Values) (*Logs, error) {
-	q, err := query.Validate(v,
-		query.WithIntParam("cursor"),
-		query.WithStringsParam("status", []string{"pending", "processing", "failure", "success"}),
-	)
+	q, err := query.Validate(v, query.IntParam("cursor"))
 	if err != nil {
 		return nil, err
 	}
 	var l Logs
-	if cursor, ok := q.Params["cursor"]; ok {
+	if cursor, ok := q["cursor"]; ok {
 		l.Cursor = cursor.(int64)
 	}
 	return &l, nil
