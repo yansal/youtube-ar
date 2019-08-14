72fac6849fbeaf5aceff4a86ddf80102b0b4f159
Yann Salaün <yann.salaun@ulule.com>
2019-08-14 13:58:02 +0200

    add tests for store/querybuilder

---
+++ api/store/querybuilder/select_test.go
@@ -0,0 +1,44 @@
+package querybuilder
+
+import "testing"
+
+func assertf(t *testing.T, ok bool, msg string, args ...interface{}) {
+	t.Helper()
+	if !ok {
+		t.Errorf(msg, args...)
+	}
+}
+
+func TestSelect(t *testing.T) {
+	for _, tt := range []struct {
+		stmt  *SelectStmt
+		query string
+		args  []interface{}
+	}{{
+		stmt:  Select(NewCallExpr("unnest", NewIndexExpr("logs", NewBindValue(1)))),
+		query: "SELECT unnest(logs[$1:])",
+		args:  []interface{}{1},
+	},
+		{
+			stmt: Select("col").From("table").Where(
+				NewBoolExpr(NewBoolExpr(
+					NewIdentifier("deleted_at").IsNull()).
+					And(NewIdentifier("id").LessThan(1))).
+					And(NewIdentifier("status").In([]string{"success", "failed"})),
+			),
+			query: "SELECT col FROM table WHERE deleted_at IS NULL AND id < $1 AND status IN ($2, $3)",
+			args:  []interface{}{1, "success", "failed"},
+		}} {
+		query, args := tt.stmt.Build()
+		assertf(t, query == tt.query, "expected %q, got %q", tt.query, query)
+		assertf(t, len(args) == len(tt.args), "expected %d args, got %d", len(tt.args), len(args))
+		minlen := len(args)
+		if len(tt.args) < minlen {
+			minlen = len(tt.args)
+		}
+		for i := 0; i < minlen; i++ {
+			assertf(t, args[i] == tt.args[i], "expected %#v, got %#v", tt.args[i], args[i])
+
+		}
+	}
+}
---
+++ api/store/querybuilder/update_test.go
@@ -0,0 +1,27 @@
+package querybuilder
+
+import "testing"
+
+func TestUpdate(t *testing.T) {
+	for _, tt := range []struct {
+		stmt  *UpdateStmt
+		query string
+		args  []interface{}
+	}{{
+		stmt:  Update("urls").Set(map[string]interface{}{"logs": NewCallExpr("array_append", "logs", NewBindValue(1))}),
+		query: "UPDATE urls SET logs = array_append(logs, $1)",
+		args:  []interface{}{1},
+	}} {
+		query, args := tt.stmt.Build()
+		assertf(t, query == tt.query, "expected %q, got %q", tt.query, query)
+		assertf(t, len(args) == len(tt.args), "expected %d args, got %d", len(tt.args), len(args))
+		minlen := len(args)
+		if len(tt.args) < minlen {
+			minlen = len(tt.args)
+		}
+		for i := 0; i < minlen; i++ {
+			assertf(t, args[i] == tt.args[i], "expected %#v, got %#v", tt.args[i], args[i])
+
+		}
+	}
+}
