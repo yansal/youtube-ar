afd1bb0b82744d7f8a0f0c9c4b97ea323b1d5e3d
Oussama Ezzayer <oussama@ezzayer.net>
2019-08-16 14:41:08 +0200

    add filter

--- frontend/src/components/VideoList.js
+++ frontend/src/components/VideoList.js
@@ -10,12 +10,20 @@ class VideoList extends React.Component {
     this.videoInput = React.createRef()
 
     this.state = {
-      list: []
+      list: [],
+      filter: 'all'
     }
   }
 
   componentDidMount() {
-    fetch(`${API_URL}/urls`).then(response => {
+    this.refresh()
+  }
+
+  refresh() {
+    const { filter } = this.state
+    const baseURL = `${API_URL}/urls`
+    let url = filter === 'all' ? baseURL : `${baseURL}?status=${filter}`
+    fetch(url).then(response => {
       response.json().then(resource => {
         this.setState({ list: resource.urls, nextCursor: resource.next_cursor })
       })
@@ -55,23 +63,33 @@ class VideoList extends React.Component {
 
         const { list } = this.state
         const updatedList = list.concat(resource.urls)
-        this.setState({ list: updatedList, nextCursor: resource.next_cursor})
+        this.setState({ list: updatedList, nextCursor: resource.next_cursor })
       })
     })
   }
 
+  handleChange = event => {
+    this.setState({ filter: event.target.value }, this.refresh)
+  }
+
   render() {
     const { list } = this.state
-    const listNodes = list.map(video => (
-      <Video key={video.id} video={video} />
-    ))
+
+    const listNodes = list && list.map(video => <Video key={video.id} video={video} />)
 
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <input type="text" ref={this.videoInput} />
         </form>
-        <ul>{listNodes}</ul>
+        <select value={this.state.filter} onChange={this.handleChange}>
+          <option value="all">All</option>
+          <option value="success">Success</option>
+          <option value="failure">Failure</option>
+          <option value="processing">Processing</option>
+          <option value="pending">Pending</option>
+        </select>
+        {list ? <ul>{listNodes}</ul> : <div>Nothing to show!</div>}
         <button onClick={this.handleNext}>Next</button>
       </div>
     )
