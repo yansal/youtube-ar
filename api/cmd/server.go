94bb4e4722c252bb98eb0f6eef3ab68d619d3989
Oussama Ezzayer <oussama@ezzayer.net>
2019-08-16 17:25:43 +0200

    allow DELETE methods from browsers

--- api/cmd/server.go
+++ api/cmd/server.go
@@ -44,6 +44,7 @@ func Server(ctx context.Context, args []string) error {
 	mux.HandleFunc(http.MethodGet, regexp.MustCompile(`^/urls/(\d+)$`), handler.DetailURL(manager, serializer))
 	mux.HandleFunc(http.MethodDelete, regexp.MustCompile(`^/urls/(\d+)$`), handler.DeleteURL(manager))
 	mux.HandleFunc(http.MethodGet, regexp.MustCompile(`^/urls/(\d+)/logs$`), handler.ListLogs(manager, serializer))
+	mux.HandleFunc(http.MethodOptions, regexp.MustCompile(`.*`), func(w http.ResponseWriter, r *http.Request){})
 
 	retrier := service.NewRetrier(broker, manager, store)
 	mux.HandleFunc(http.MethodPost, regexp.MustCompile(`^/urls/(\d+)/retry$`), handler.RetryDownloadURL(retrier, serializer))
